1fadf3274dbde9f2d10515282e820362ca1bf2d8
Karol M <keszua@gmail.com>
2023-03-05 23:06:51 +0100

    Validate the common type

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { TestInterface } from 'types';
 
 function App() {
+  const jakasStala: TestInterface = {
+    test: 'blabla',
+  }
+
+
   return (
     <div className="App">
       <header className="App-header">
