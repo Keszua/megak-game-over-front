ea947c417c9c9cb225a6b48d4c71cc96564f808d
Karol M <keszua@gmail.com>
2023-04-06 15:12:50 +0200

    Add spiner

---
+++ src/components/common/Spiner/SpinerCandle.tsx
@@ -0,0 +1,9 @@
+import './SpinerCandle.css';
+import { CandleSVG } from "./CandleSVG";
+
+
+export const SpinerCandle = () => (
+    <div className="SpinerCandle">
+        <CandleSVG />
+    </div>
+)
